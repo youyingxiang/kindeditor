acf12518547a867caac0bc450a7a7781809c7d08
yxx <1365831278@qq.com>
2019-05-20 22:48:08 +0800

    增加上传视音频

--- src/config/editor.php
+++ src/config/editor.php
@@ -18,8 +18,10 @@ return [
     'up_config' => [
         "image_size"    => 1024*1024*3,                                                 // 上传图片大小
         "file_size"     => 1024*1024*100,                                               // 上传文件大小
+        "media_size"    => 1024*1024*100,                                               // 上传视音频大小
         "image_format"  => "jpg,gif,jpeg,png,bmp,svg",                                  // 上传图片格式
         "file_format"   => "doc,docx,xls,xlsx,ppt,htm,html,txt,rar,zip,mp4,pdf,pptx",   // 上传文件格式
-        "upload_path"   => "uploads",                                           // 上传文件目录
+        "media_format"  => "mp3,mp4,avi",                                               // 上传视音频格式
+        "upload_path"   => "uploads",                                                   // 上传文件目录
     ],
 ];
